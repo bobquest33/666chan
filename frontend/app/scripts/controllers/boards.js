8bfca43ddd36918c3d1af567ffe11566951f2ee7
Pedro Vanzella <pedro@pedrovanzella.com>
2015-07-03 09:23:27 -0300

    Error message if board cannot be added.

--- frontend/app/scripts/controllers/boards.js
+++ frontend/app/scripts/controllers/boards.js
@@ -6,12 +6,18 @@ angular.module('frontendApp')
             $scope.boards = data.boards;
         });
 
+        $scope.error = "";
+
         $scope.board = new Board();
         $scope.addBoard = function(board) {
             var new_board = angular.copy($scope.board);
             $scope.board.$save(function(u, headers) {
                 if (u.status == "okay") {
                     $scope.boards.push(new_board);
+                } else if (u.status == "duplicate entry") {
+                    $scope.error = "Board already exists!";
+                } else if (u.status == "missing args") {
+                    $scope.error = "A Board needs at least a Shortname and a Name.";
                 }
             });
         };
